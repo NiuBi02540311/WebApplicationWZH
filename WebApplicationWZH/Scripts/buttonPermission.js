c481547846614a7aeea9bc99e2e3c3fa356727a7
NiuBi02540311 <qhjio@163.com>
2023-10-29 14:14:23 +0800

    14.14

--- WebApplicationWZH/Scripts/buttonPermission.js
+++ WebApplicationWZH/Scripts/buttonPermission.js
@@ -2,7 +2,7 @@
 
 setTimeout(function () {
     check();
-}, 1000);
+}, 500);
 
 function check() {
     const pathname = window.location.pathname;
@@ -24,9 +24,12 @@ function check() {
             if (xhr.status == 200) {
                 //alert(xhr.responseText);
                 //console.log(xhr);
-                console.log(xhr.responseText);
+                //console.log(xhr.responseText);
                 let obj = JSON.parse(xhr.responseText);
                 if (obj == null || obj == undefined) {
+                    return;
+                }
+                if (obj.length == 0) {
                     return;
                 }
                 console.log(obj);
@@ -34,11 +37,16 @@ function check() {
                     //console.log(item.ButtonID, item.Permission);
                     //document.getElementById(item.ButtonID).disabled = item.Permission;
                     //document.getElementById(item.ButtonID).style.display = item.Permission ? "block" : "none";
+                    let ele = document.getElementById(item.ButtonID);
+                    if (ele != null && ele != undefined) {
+                        //let display = document.getElementById(item.ButtonID).style.display;
+                        //document.getElementById(item.ButtonID).style.display = item.Permission ? display: "none";// 隐藏不可见
+                        //document.getElementById(item.ButtonID).disabled = item.Permission;// 禁用
 
-                    let display = document.getElementById(item.ButtonID).style.display;
-                    //document.getElementById(item.ButtonID).style.display = item.Permission ? display: "none";// 隐藏不可见
-
-                    document.getElementById(item.ButtonID).disabled = item.Permission;// 禁用
+                        //let display = ele.style.display;
+                        //ele..style.display = item.Permission ? display: "none";// 隐藏不可见
+                        ele.disabled = item.Permission;// 禁用
+                    }
                 }
 
             } else {
