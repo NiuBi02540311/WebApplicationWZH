e4b1c200bb7e572e357c033034f8d55800a3437f
NiuBi02540311 <qhjio@163.com>
2023-11-06 23:29:20 +0800

    23.29

--- WebApplicationWZH/Controllers/RoleController.cs
+++ WebApplicationWZH/Controllers/RoleController.cs
@@ -19,16 +19,38 @@ namespace WebApplicationWZH.Controllers
         //[ValidateAntiForgeryToken]
         [SkipCheckLogin]
         [SkipVerification]
-        public ActionResult GetRoleUser()
+        public string GetRoleUser()
         {
             string gridpager = HttpContext.Request.Params["gridPager"];
-            gridPager grid = Newtonsoft.Json.JsonConvert.DeserializeObject<gridPager>(gridpager);
+            GridRequestModel grid = Newtonsoft.Json.JsonConvert.DeserializeObject<GridRequestModel>(gridpager);
             var find = DB.SqlServer.Select<Users>().Where(a => a.IsDelete == 0).ToList();
-            return Json(find);
+
+            int pageCount = find.Count / grid.pageSize;
+            if (find.Count % grid.pageSize != 0)
+            {
+                pageCount++;
+            }
+
+            //GridResponseModel res =   new  GridResponseModel<Users>(find);
+            var v = find.Skip((grid.nowPage - 1) * grid.pageSize).Take(grid.pageSize).ToList();
+            //var v = find.Skip(grid.nowPage  * grid.pageSize).Take(grid.pageSize).ToList();
+            var res = new GridResponseModel<Users>();
+            res.exhibitDatas = v;
+            res.isSuccess = true;
+            res.nowPage = grid.nowPage;
+            res.recordCount = find.Count;
+            res.pageSize = grid.pageSize;
+            res.pageCount = pageCount;
+
+            return Newtonsoft.Json.JsonConvert.SerializeObject(res);
+            //return Json(res);
+
+            // 2. DataTable 转 List
+            //List<CustomerContact> list2 = DtToList<CustomerContact>.ConvertToModel(dt);
         }
     }
 
-    public class gridPager
+    public class GridRequestModel
     {
         //{"isExport":false,"pageSize":10,"startRecord":0,"nowPage":1,"recordCount":-1,"pageCount":-1,"parameters":{},"fastQueryParameters":{},"advanceQueryConditions":[],"advanceQuerySorts":[]}
         //https://os.dlshouwen.com/grid/doc/i18n/zh-cn/example.html#2.2.2
@@ -49,8 +71,60 @@ namespace WebApplicationWZH.Controllers
 
         public object advanceQueryConditions { get; set; }
         public object advanceQuerySorts { get; set; }
+
+        public List<object> exhibitDatas { get; set; }
+
+        public bool exportAllData { get; set; } = false;
+
+        public List<object> exportColumns { get; set; }
+
+        public bool exportDataIsProcessed { get; set; } = false;
     }
 
+    public class GridResponseModel<T> where T : new()
+    {
+        //{"isExport":false,"pageSize":10,"startRecord":0,"nowPage":1,"recordCount":-1,"pageCount":-1,"parameters":{},"fastQueryParameters":{},"advanceQueryConditions":[],"advanceQuerySorts":[]}
+        //https://os.dlshouwen.com/grid/doc/i18n/zh-cn/example.html#2.2.2
+        public bool isExport { get; set; }
+        public int pageSize { get; set; }
+        public int startRecord { get; set; }
+
+        public int nowPage { get; set; }
+        public int recordCount { get; set; }
+        public int pageCount { get; set; }
+
+        public parameters parameters { get; set; } = new parameters();
+
+        public fastQueryParameters fastQueryParameters { get; set; } = new fastQueryParameters();
+
+        public object advanceQueryConditions { get; set; } = new List<object>();
+        public object advanceQuerySorts { get; set; } = new List<object>();
+
+        public List<T> exhibitDatas { get; set; }
+
+      
+
+        public bool exportAllData { get; set; } = false;
+
+        public List<object> exportColumns { get; set; } = new List<object>();
+
+        public bool exportDataIsProcessed { get; set; } = false;
+
+        public List<object> exportDatas { get; set; } = new List<object>();
+
+        public string exportFileName { get; set; } = "";
+
+        public string exportType { get; set; } = "";
+
+        public bool isSuccess { get; set; } = true;
+ 
+    }
+    public class parameters
+    {
+    }
+    public class fastQueryParameters
+    {
+    }
     /*
      查询返回
      {
